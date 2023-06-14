1b6b07ffe52aa1d89cfd01f192a0b97aaed15fcd
vectorell <vlindell@outlook.com>
2023-06-14 17:32:12 +0200

    test for render

--- Pratbubblan/backend/utils/db.js
+++ Pratbubblan/backend/utils/db.js
@@ -5,7 +5,7 @@ export async function connectDb() {
     dotenv.config()
     const db = process.env.mongoURI
     try {
-        await mongoose.connect(db, {useNewUrlParser: true}) // ger tillbaka ett promise
+        await mongoose.connect(process.env.mongoURI, {useNewUrlParser: true}) // ger tillbaka ett promise
         console.log('MongoDB ansluten..')
     } catch (error) {
         console.error(error.message)
